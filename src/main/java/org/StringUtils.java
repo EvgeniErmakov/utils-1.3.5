d30de189d10d46ebcb3bfb130aaca249b59ee888
Evgeni Ermakov <evgeni_ermakov@Mail.ru>
2021-10-22 22:01:34 +0300

    feat: add new opportunity

--- src/main/java/org/StringUtils.java
+++ src/main/java/org/StringUtils.java
@@ -4,6 +4,9 @@ import org.apache.commons.lang3.math.NumberUtils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        return NumberUtils.isParsable(str);
+        if (NumberUtils.isParsable(str)) {
+            return NumberUtils.createDouble(str) > 0;
+        }
+        return false;
     }
 }
--- src/test/java/StringUtilsTest.java
+++ src/test/java/StringUtilsTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -14,14 +15,17 @@ public class StringUtilsTest {
 
     static Stream<Arguments> provideArguments() {
         return Stream.of(
-                Arguments.of("0", true),
+                Arguments.of("0", false),
                 Arguments.of("1990", true),
-                Arguments.of("-1990", true),
-                Arguments.of("-9223372036854775808", true),
+                Arguments.of("-1990", false),
+                Arguments.of("-9223372036854775808", false),
                 Arguments.of("9223372036854775807", true),
                 Arguments.of("Hi", false),
                 Arguments.of(null, false),
-                Arguments.of("", false)
+                Arguments.of("", false),
+                Arguments.of("123.123", true),
+                Arguments.of("-123.123", false),
+                Arguments.of("123,123", false)
         );
     }
 
