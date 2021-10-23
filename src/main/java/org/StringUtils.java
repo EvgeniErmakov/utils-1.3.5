5c95478eb76e45e57ed527cf0a03b2deb75c6a30
Evgeni Ermakov <evgeni_ermakov@Mail.ru>
2021-10-23 11:56:36 +0300

    feat: add comment

--- src/main/java/org/StringUtils.java
+++ src/main/java/org/StringUtils.java
@@ -7,6 +7,7 @@ public class StringUtils {
         if (NumberUtils.isParsable(str)) {
             return NumberUtils.createDouble(str) > 0;
         }
+        // text for testing fetch function();
         return false;
     }
 }
